eee90a3d111ecac9b62cf8a838661dab737ddb06
Navjot Singh Virk <virksaabnavjot@gmail.com>
2017-04-20 23:23:41 +0100

    Projector Class completed

--- code/NSVSmartMeetingSpace/NSVSmartMeetingSpace/src/main/java/nsv/smartmeetingspace/Printer.java
+++ code/NSVSmartMeetingSpace/NSVSmartMeetingSpace/src/main/java/nsv/smartmeetingspace/Printer.java
@@ -2,7 +2,8 @@ package nsv.smartmeetingspace;
 
 /**
  *
- * @author Navjot Singh Class description: This class describes the printer
+ * @author Navjot Singh 
+ * Class description: This class describes the printer
  * device and the functionality it supports
  */
 public class Printer {
--- code/NSVSmartMeetingSpace/NSVSmartMeetingSpace/src/main/java/nsv/smartmeetingspace/Projector.java
+++ code/NSVSmartMeetingSpace/NSVSmartMeetingSpace/src/main/java/nsv/smartmeetingspace/Projector.java
@@ -1,10 +1,68 @@
 package nsv.smartmeetingspace;
 
+import java.util.List;
+
 /**
  *
- * @author Navjot Singh
- * Class description: This class describes the projector device and the functionality it supports
+ * @author Navjot Singh Class description: This class describes the projector
+ * device and the functionality it supports
  */
 public class Projector {
-    
+
+    private String deviceName, deviceLocation, resolution, currentlyProjectedFile;
+    private boolean switchedOn;
+    private List<String> connectionType;
+
+    public Projector() {
+
+    }
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    public void setDeviceName(String deviceName) {
+        this.deviceName = deviceName;
+    }
+
+    public String getDeviceLocation() {
+        return deviceLocation;
+    }
+
+    public void setDeviceLocation(String deviceLocation) {
+        this.deviceLocation = deviceLocation;
+    }
+
+    public String getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(String resolution) {
+        this.resolution = resolution;
+    }
+
+    public String getCurrentlyProjectedFile() {
+        return currentlyProjectedFile;
+    }
+
+    public void setCurrentlyProjectedFile(String currentlyProjectedFile) {
+        this.currentlyProjectedFile = currentlyProjectedFile;
+    }
+
+    public boolean isSwitchedOn() {
+        return switchedOn;
+    }
+
+    public void setSwitchedOn(boolean switchedOn) {
+        this.switchedOn = switchedOn;
+    }
+
+    public List<String> getConnectionType() {
+        return connectionType;
+    }
+
+    public void setConnectionType(List<String> connectionType) {
+        this.connectionType = connectionType;
+    }
+
 }

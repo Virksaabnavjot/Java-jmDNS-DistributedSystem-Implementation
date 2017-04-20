0ebb8e5b0f5b7eb5ee730716d3d3e86518b7069b
Soffyan Ali <soffyanali@gmail.com>
2017-04-20 21:34:56 +0100

    Adding Mobile Phone Device

---
+++ code/NSVSmartMeetingSpace/NSVSmartMeetingSpace/src/main/java/nsv/smartmeetingspace/MobilePhone.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nsv.smartmeetingspace;
+
+/**
+ *
+ * @author Soffyan Ali
+ * Class description: This class describes the Mobile Phone and the functionality it supports
+ */
+public class MobilePhone {
+    
+}

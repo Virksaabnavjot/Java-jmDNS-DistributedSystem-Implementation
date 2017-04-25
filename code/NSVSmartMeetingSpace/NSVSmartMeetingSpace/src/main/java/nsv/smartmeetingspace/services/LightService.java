8fc8c90ad1346008abea8159a569311176ef582d
Navjot Singh Virk <virksaabnavjot@gmail.com>
2017-04-25 02:55:45 +0100

    Creating JmDNS instance and registering service - Light Service class

--- code/NSVSmartMeetingSpace/NSVSmartMeetingSpace/src/main/java/nsv/smartmeetingspace/services/LightService.java
+++ code/NSVSmartMeetingSpace/NSVSmartMeetingSpace/src/main/java/nsv/smartmeetingspace/services/LightService.java
@@ -1,6 +1,12 @@
 package nsv.smartmeetingspace.services;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.Arrays;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceInfo;
 import nsv.smartmeetingspace.Light;
 
 /**
@@ -12,10 +18,25 @@ import nsv.smartmeetingspace.Light;
 public class LightService {
 
     private Light light;
+    private static JmDNS jmDNS;
 
     //constructor
     public LightService() {
         light = new Light(50, true, Arrays.asList("Cool", "Bright DayLight, Dark"));
     }
 
+    public static void main(String args[]) {
+        //error handling through try-catch
+        try {
+            //creating a JmDNS instance
+            jmDNS = JmDNS.create(InetAddress.getLocalHost());
+            
+            //regestering a service
+            ServiceInfo si = ServiceInfo.create("light.tcp.local.", "Light", 1111, "Navsingh 40W Bulb");
+            jmDNS.registerService(si);
+            
+        } catch (IOException ex) {
+            Logger.getLogger(LightService.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
